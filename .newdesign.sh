b0ea6bbd85d4aec545b420abd65b8fcc2dfea081
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-04-30 22:27:29 -0500

    Add initial files and begin README

---
+++ .newdesign.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+echo What is the Design project\'s name? !!!REMINDER!!! No Spaces!
+
+read projectname
+
+mkdir -p ~/Design/$projectname/{01_Assets/{Photography,Inspiration,Typography},02_Design,03_Proofs,04_Delivered}
