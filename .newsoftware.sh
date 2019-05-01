5370f158df272119cdeb5006a529b98b593ac568
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-04-30 22:56:14 -0500

    Add script files and general README outline

---
+++ .newsoftware.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+echo What is the Software project\'s name? ***REMINDER*** !No Spaces!
+
+
+read projectname
+
+mkdir -p ~/Dev/Software/$projectname/{01_Assets/{Photography,Inspiration,Typography},02_Design,03_Code,04_Proofs,05_Delivered}
